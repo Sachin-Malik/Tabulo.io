0191b25507a3df4897aa6353b9873987af16417a
Sachin Malik <sachinmalik199821@gmail.com>
2022-01-13 16:56:04 +0530

    Add Basic Pagination

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import {useState} from 'react';
+
 import Table from './Components/Table'
 import Items from './Items'
+import Pagination from './Components/Pagination'
 function App() {
+
+  const [currentPage,setCurrentPage]=useState(1);
+  const [itemsPerPage,setItemsPerPate]=useState(10);
+
+  //Get Current Items
+  const indexOfLastItem= currentPage*itemsPerPage;
+  const indexOfFirstItem= indexOfLastItem-itemsPerPage;
+  const currentItems = Items.slice(indexOfFirstItem,indexOfLastItem);
+
+  //change page
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  }
+
   return (
     <div className="container">
       <h1 className="mt-5 text-center">Tabulo.io</h1>
-       <Table items={Items}/>
+       <Table items={currentItems}/>
+       <Pagination itemsPerPage={itemsPerPage} totalItems={Items.length} paginate={paginate}/>
     </div>
   );
 }
---
+++ src/Components/Pagination.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Pagination({itemsPerPage,totalItems,paginate}) {
+    const pageNumber =[];
+
+    for(let i=1;i<=Math.ceil(totalItems/itemsPerPage);i++){
+        pageNumber.push(i);
+    };
+
+    return (
+        <nav>
+            <ul className="pagination">
+            {pageNumber.map((number,id)=>(
+               <li key={id} className='page-item'>
+                   <a href='!#' onClick={()=>paginate(number)} className='page-link'>{number}</a>
+               </li>
+            ))}
+            </ul>
+        </nav>
+    )
+}
+
+export default Pagination
